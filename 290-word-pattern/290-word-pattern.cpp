152815741edd32b3f87975993b40a18fc0f338db
Sejal Rai <sejalrai23@gmail.com>
2022-01-17 13:22:05 +0530

    Time: 6 ms (5.51%), Space: 6.3 MB (82.42%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char,string> m;
+        istringstream iss(s);
+        int i=0;
+        set<char> p1;
+        set<string> s2;
+        while(iss && i<pattern.length()){
+            string fin;
+            iss>>fin;
+            // cout<<fin<<"-"<<pattern[i]<<endl;
+            // m[pattern[i]]=fin;
+            m.insert({pattern[i],fin});
+            p1.insert(pattern[i]);
+            s2.insert(fin);
+            i++;
+            
+        }
+        if((p1.size()!=s2.size()) ){
+            return false;
+        }
+        string ans="";
+        for(int i=0;i<pattern.length();i++){
+            auto it=m.find(pattern[i]);
+            if(it!=m.end()){
+                ans+=it->second;
+                i!=pattern.length()-1 ? ans+=" ": ans;
+            }
+            
+        }
+        // cout<<ans;
+        
+        return ans==s;
+        
+    }
+};
