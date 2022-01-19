b0ed248603a9e3c3523cc2bf65582c7034a19e50
Sejal Rai <sejalrai23@gmail.com>
2022-01-19 16:47:31 +0530

    Time: 10 ms (43.67%), Space: 7.6 MB (73.74%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -32,3 +32,25 @@ public:
         
     
 };
+
+// TC=O(N)
+//SC=O(N)
+
+// To understand this solution, you just need to ask yourself this question.
+// Assume the distance from head to the start of the loop is x1
+// the distance from the start of the loop to the point fast and slow meet is x2
+// the distance from the point fast and slow meet to the start of the loop is x3
+
+// What is the distance fast moved? What is the distance slow moved? And their relationship?
+
+// x1 + x2 + x3 + x2
+// x1 + x2
+// x1 + x2 + x3 + x2 = 2 (x1 + x2)
+// Q: Why x1 + x2 + x3 + x2 = 2 (x1 + x2)?
+
+// Ans: Its just the relationship between the distance travelled by fast ptr and the distance travelled by the slow ptr.
+// As fast ptr, runs twice the speed of slow ptr. Therefore, when they meet, distance travelled by fast ptr will be 2*(distance travelled by slow ptr).
+
+// Therefore, x1 = x3.
+
+// Hope this makes sense.
