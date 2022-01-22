2af430f0a652296273043b78ec484e6f757bf770
Sejal Rai <sejalrai23@gmail.com>
2022-01-22 13:20:22 +0530

    Time: 24 ms (87.25%), Space: 6.3 MB (89.60%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n+1);
+        dp[0]=false;
+        dp[1]=true;
+        for (int i=2;i<=n;i++){
+            for(int j=1; j*j<=i;j++){
+                if(dp[i-j*j]==false){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+        
+    }
+};
