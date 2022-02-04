ca46ebff5cea6ecb42c61615ce63ac26001b3b80
Sejal Rai <sejalrai23@gmail.com>
2022-02-04 13:08:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (49.92%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int dp[n+1];
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 1;
+        }else{
+            dp[0]=0;
+            dp[1]=1;
+            dp[2]=1;
+            for(int i= 3 ;i<=n ; i++){
+            dp[i]= dp[i-1]+dp[i-2]+dp[i-3];
+            // cout<<i<<"-"<<dp[i]<<endl;
+        }
+        }
+        
+        return dp[n];
+        
+    }
+};
