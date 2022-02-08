d0b010122f8738f51ce5543e06d0804901afeca7
Sejal Rai <sejalrai23@gmail.com>
2022-02-08 13:30:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,10 +1,25 @@
+// class Solution {
+// public:
+//     int addDigits(int num) {
+//         while(!(num<=9 && num>=0)){
+//             num= num%10 + num/10;
+            
+//         }
+//         return num;
+//     }
+// };
+
+//TC= O(logn)
+
 class Solution {
 public:
     int addDigits(int num) {
-        while(!(num<=9 && num>=0)){
-            num= num%10 + num/10;
-            
-        }
-        return num;
+      if(num==0)
+          return 0;
+      else if(num%9==0)
+          return 9;
+      else
+          return num%9;
     }
 };
+// UPDATE : The O(1) approach method is called Digit Root. Check it out.
