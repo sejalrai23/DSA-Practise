615ad07c0412a251ebcaffb934a43765dc3b0e79
Sejal Rai <sejalrai23@gmail.com>
2022-02-09 21:25:05 +0530

    Time: 234 ms (12.48%), Space: 64.6 MB (58.79%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -9,23 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void sumBST(TreeNode* root , int low , int high , int& sum){
+//         if(root==NULL){
+//             return ;
+//         }
+//         sumBST(root->left,low,high,sum);
+//         if(root->val >=low && root->val <=high ){
+//             sum+=root->val;
+//         }
+//         sumBST(root->right,low , high,sum);
+        
+//     }
+//     int rangeSumBST(TreeNode* root, int low, int high) {
+//         int sum=0;
+//         sumBST(root, low , high , sum);
+//         return sum;
+        
+//     }
+// };
 class Solution {
 public:
-    void sumBST(TreeNode* root , int low , int high , int& sum){
-        if(root==NULL){
-            return ;
-        }
-        sumBST(root->left,low,high,sum);
-        if(root->val >=low && root->val <=high ){
-            sum+=root->val;
-        }
-        sumBST(root->right,low , high,sum);
-        
-    }
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-        sumBST(root, low , high , sum);
-        return sum;
-        
+    int rangeSumBST(TreeNode* root, int L, int H) {
+        if(!root) return 0;
+        int ans = root -> val >= L && root -> val <= H ? root -> val : 0;
+        if(root -> val > L) ans += rangeSumBST(root -> left, L, H);
+        if(root -> val < H) ans += rangeSumBST(root -> right, L, H);
+        return ans;
     }
 };
