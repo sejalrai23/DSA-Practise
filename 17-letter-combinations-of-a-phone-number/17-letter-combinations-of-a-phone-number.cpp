632cc0b515bdd6e4f27539401ce4c8d6d826ee56
Sejal Rai <sejalrai23@gmail.com>
2022-02-18 13:57:44 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (34.93%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
  vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;    
  vector<string> letterCombinations(string digits) {
-	if(digits == "") return ans;         // no combinations formed without any digits
-	helper(digits, 0, "");              
+	if(digits == "") return ans;  
+     string combi="";// no combinations formed without any digits
+	helper(digits, 0, combi);              
 	return ans;
 }
-// recursive helper function
-void helper(string &digits, int i, string combi){	
-	if(i == size(digits))   // base condition - push when we have recursed for all digits
+
+void helper(string &digits, int i, string &combi){	
+	if(i == size(digits))  
 		ans.push_back(combi);    
-	else // add and recurse for each character (chosen from current digit-charcter mapping)
+	else 
 		for(auto c : mappings[digits[i] - '2']){
             combi+=c;
             helper(digits, i + 1, combi );
             combi.pop_back();
-        } // <= -2 bcoz digits are in range ['2','9']. We need to convert them to indices from 0 to 8 for indexing mappings.
+        }
 			
 }
 };
