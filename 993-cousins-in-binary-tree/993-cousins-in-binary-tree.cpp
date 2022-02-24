167ccaef961019a8ab1b03e78326ef4cf7885769
Sejal Rai <sejalrai23@gmail.com>
2022-02-25 00:00:02 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (7.17%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -17,9 +17,7 @@ public:
             return ;
         }
         if(root->val == node){
-            // cout<<c<<endl;
             v.push_back({c,par->val});
-            // count=c;
             return ;  
         }
         findDepth(root->left,node , c+1,count ,root);
@@ -33,11 +31,6 @@ public:
         int x1=0,x2=0;
         findDepth(root,x,0,x1,root);
         findDepth(root,y,0,x2,root);
-
-        // cout<<x1<<"-"<<x2<<endl;
-        // for(auto it :v){
-        //     cout<<it.first <<"-"<<it.second->val<<endl;
-        // }
         if(v[0][0]!=v[1][0] || v[0][1]==v[1][1]){
             return false;
         }
@@ -45,3 +38,4 @@ public:
         
     }
 };
+
