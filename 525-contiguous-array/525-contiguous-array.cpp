92385c51627019cf357a8cac9bdc4f813ec667e2
Sejal Rai <sejalrai23@gmail.com>
2022-02-28 12:41:10 +0530

    Time: 210 ms (41.26%), Space: 84.3 MB (19.75%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -6,12 +6,9 @@ public:
         m[0]=-1;
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                nums[i]=-1;
-            }
-            sum+=nums[i];
+            sum+=nums[i]==0 ? -1 :1;
             if(m.find(sum)!=m.end()){
-                int len= i-m[sum];
+                int len=i-m[sum];
                 if(ans<len){
                     ans=len;
                 }
