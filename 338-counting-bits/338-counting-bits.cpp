78340d9ebc89857c067a20e763766e8dd3575930
Sejal Rai <sejalrai23@gmail.com>
2022-03-01 06:38:58 +0530

    Time: 4 ms (88.16%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
         ans[0]=0;
+        int k=1;
         for(int i=1;i<=n;i++){
-            if(i%2==0){
-                ans[i]=ans[i/2];
-            }else{
-                ans[i]=ans[i-1]+1;
+            if(2*k==i){
+                k*=2;
             }
+            ans[i]=ans[i-k]+1;
         }
         return ans;
     }
