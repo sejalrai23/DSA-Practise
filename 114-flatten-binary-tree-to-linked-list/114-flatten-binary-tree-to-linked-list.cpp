0d868566f1b7de4f3e25324ec5157e8318361da9
Sejal Rai <sejalrai23@gmail.com>
2022-03-03 00:44:47 +0530

    Time: 18 ms (9.65%), Space: 12.9 MB (18.94%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -9,19 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* prev= NULL;
+//     void flatten(TreeNode* root) {
+//         if(root==NULL){
+//             return;
+//         }
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->right=prev;
+//         root->left=NULL;
+//         prev=root;
+       
+        
+//     }
+// };
+
+//iterative 
+
 class Solution {
 public:
-    TreeNode* prev= NULL;
-    void flatten(TreeNode* root) {
+    void flatten(TreeNode* root){
         if(root==NULL){
             return;
         }
-        flatten(root->right);
-        flatten(root->left);
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
-       
-        
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* cur=s.top();
+            s.pop();
+            if(cur->right) s.push(cur->right);
+            if(cur->left) s.push(cur->left);
+            if(!s.empty()){
+                cur->right=s.top();
+            }
+            cur->left=NULL;
+        }
     }
+    
 };
