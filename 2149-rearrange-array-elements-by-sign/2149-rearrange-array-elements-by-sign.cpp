9ad835e880556bd9fa1f3c7e7b8121cb0748eadf
Sejal Rai <sejalrai23@gmail.com>
2022-03-03 00:33:55 +0530

    Time: 193 ms (92.18%), Space: 123.4 MB (99.73%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<int> ans(nums.size());
         int p=0,n=1;
         for(int i=0;i<nums.size();i++){
