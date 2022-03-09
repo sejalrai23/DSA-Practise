f863911a8b52423620db7ed83011905b45f688bd
Sejal Rai <sejalrai23@gmail.com>
2022-03-09 12:29:24 +0530

    Time: 4 ms (32.67%), Space: 5.9 MB (41.37%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        int mod=1e9+7;
+        long long ans=1;
+        for(int i=1;i<=n;i++){
+            ans= (ans*i)%mod;
+            ans= (ans*(2*i-1))%mod;
+        }
+        return ans;
+    }
+};
