36bd3367f73a6609d8a2e1b50631157424a48178
Sejal Rai <sejalrai23@gmail.com>
2022-03-10 08:01:26 +0530

    Time: 28 ms (90.59%), Space: 71.6 MB (11.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -8,40 +8,6 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* add(ListNode* l1 , ListNode* l2, int c){
-//         ListNode* ans = new ListNode();
-//        if (l1 == NULL and l2 == NULL) {
-//             if(c>0)return new ListNode(c);
-//             return NULL;
-          
-         
-//        }
-// 	   else if (l1 == NULL) return ans->next=add(new ListNode(0), l2, c);
-// 	   else if (l2 == NULL)  return ans->next=add(l1, new ListNode(0), c); 
-        
-        
-//         if(c+l1->val+l2->val > 9){
-//             ans->val= (c+l1->val+l2->val)%10;
-//             c= (c+l1->val+l2->val)/10;
-//         }else {
-//             ans->val=c+l1->val+l2->val;
-//         }
-//         cout<<l1->val << "-"<< l2->val << "-"<<c<<"-"<<ans->val<<endl;
-//         ans->next = add(l1->next,l2->next,c);
-//         return ans;
-         
-       
-        
-//     }
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//        return add(l1,l2,0);
-       
-        
-        
-//     }
-// };
 
 class Solution {
 public:
