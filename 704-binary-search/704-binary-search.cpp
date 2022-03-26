98ab6b88ebe55322da11cc8afc7ab914b027619a
Sejal Rai <sejalrai23@gmail.com>
2022-03-26 10:44:10 +0530

    Time: 47 ms (62.34%), Space: 27.5 MB (63.85%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        while(i<=j){
+            int mid= (i+j)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return -1;
+        
+    }
+};
