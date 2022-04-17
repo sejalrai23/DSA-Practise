353c584c5e64126fb610d11ffa154304ae49be87
Sejal Rai <sejalrai23@gmail.com>
2022-04-17 11:07:53 +0530

    Time: 4 ms (30.51%), Space: 6.6 MB (68.27%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int> > generateMatrix(int n) {
-        vector<vector<int> > vv(n, vector<int>(n));
+        vector<vector<int> > ans(n, vector<int>(n));
     
-        int rowStart = 0, rowEnd = n - 1;
-        int colStart = 0, colEnd = n - 1;
+        int left=0, right=n-1, top=0, bottom=n-1;
         int cnt = 1;
     
-        while(rowStart <= rowEnd && colStart <= colEnd)
+        while(left <= right && top <= bottom)
         {
-            for(int i = colStart; i<= colEnd; i++)
-                vv[rowStart][i] = cnt++;
-            rowStart++;
+            for(int i = left; i<= right; i++)
+                ans[top][i] = cnt++;
+            top++;
         
-            for(int i = rowStart; i<= rowEnd; i++)
-                vv[i][colEnd] = cnt++;
-            colEnd--;
+            for(int i = top; i<= bottom; i++)
+                ans[i][right] = cnt++;
+            right--;
         
-            for(int i = colEnd; i>= colStart; i--)
-                vv[rowEnd][i] = cnt++;
-            rowEnd--;
+            for(int i = right; i>= left; i--)
+                ans[bottom][i] = cnt++;
+            bottom--;
         
-            for(int i = rowEnd; i>= rowStart; i--)
-                vv[i][colStart] = cnt++;
-            colStart++;
+            for(int i = bottom; i>= top; i--)
+                ans[i][left] = cnt++;
+            left++;
         }
     
-        return vv;
+        return ans;
     }
 };
