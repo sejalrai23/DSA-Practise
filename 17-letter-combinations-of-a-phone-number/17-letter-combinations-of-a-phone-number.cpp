968c433786153baf0109adf4bfb0698930cd9ade
Sejal Rai <sejalrai23@gmail.com>
2022-05-09 17:06:19 +0530

    Time: 3 ms (38.29%), Space: 6.6 MB (35.85%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,22 +1,47 @@
+// class Solution {
+// public:
+//  vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;    
+//  vector<string> letterCombinations(string digits) {
+// 	if(digits == "") return ans;  
+//      string combi="";// no combinations formed without any digits
+// 	helper(digits, 0, combi);              
+// 	return ans;
+// }
+
+// void helper(string &digits, int i, string &combi){	
+// 	if(i == size(digits))  
+// 		ans.push_back(combi);    
+// 	else 
+// 		for(auto c : mappings[digits[i] - '2']){
+//             combi+=c;
+//             helper(digits, i + 1, combi );
+//             combi.pop_back();
+//         }
+			
+// }
+// };
+
 class Solution {
 public:
  vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;    
  vector<string> letterCombinations(string digits) {
-	if(digits == "") return ans;  
-     string combi="";// no combinations formed without any digits
-	helper(digits, 0, combi);              
+	if(digits == "") return ans;         
+	helper(digits, 0, "");              
 	return ans;
 }
 
-void helper(string &digits, int i, string &combi){	
-	if(i == size(digits))  
-		ans.push_back(combi);    
-	else 
+void helper(string &digits, int i, string combi){	
+	if(i == size(digits)) {
+        cout<<combi<<endl;
+		ans.push_back(combi); 
+    } 
+           
+	else {
 		for(auto c : mappings[digits[i] - '2']){
-            combi+=c;
-            helper(digits, i + 1, combi );
-            combi.pop_back();
-        }
+            // combi+=c;
+            helper(digits, i + 1, combi+c );
+        } 
+    }
 			
 }
 };
