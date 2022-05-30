105ad1a48bf689de9fb870a6b9e0395e4a60c968
Sejal Rai <sejalrai23@gmail.com>
2022-05-30 13:57:58 +0530

    Time: 549 ms (34.44%), Space: 114.3 MB (39.15%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int maxi=0,i=0,j=0,st=0, end=0,sum=0;
+        int maxi=0,i=0,j=0,st=0, end=0,sum=0,sum1=0;
         unordered_map<int,int> ump;
         while(j<nums.size()){
             ump[nums[j]]++;
-            // sum+=nums[j];
+            sum+=nums[j];
             if(j-i+1<ump.size()){
                 j++;
             }else if(j-i+1==ump.size()){
-                int sum1=0;
-                for(int s=i;s<=j;s++){
-                    sum1+=nums[s];
-                }
-                sum=max(sum1,sum);
+                sum1=max(sum,sum1);
                 j++;
             }else if(j-i+1>ump.size()){
                 while(j-i+1>ump.size()){
                     ump[nums[i]]--;
+                    sum-=nums[i];
                     if(ump[nums[i]]==0){
                         ump.erase(nums[i]);
                     }
+                    // sum1=max(sum,sum1);
                     i++;
+                    
                 }
                 j++;
             }
@@ -30,7 +29,7 @@ public:
         //     sum+=nums[s];
         // }
         // cout<<st<<"-"<<end<<endl;
-        return sum;
+        return sum1;
         
     }
 };
