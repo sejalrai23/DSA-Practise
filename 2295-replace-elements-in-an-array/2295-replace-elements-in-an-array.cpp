83a0c87b65f4ce6732035d79ef604ec3080e10b5
Sejal Rai <sejalrai23@gmail.com>
2022-06-05 14:17:09 +0530

    Time: 1065 ms (14.29%), Space: 142.7 MB (14.29%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i;
+        }
+        for(auto it :operations){
+            nums[mp[it[0]]]=it[1];
+            mp[it[1]]=mp[it[0]];
+        }
+        return nums;
+    }
+};
