8d0392fbeed656118877b960c73ba29bddfb9042
Sejal Rai <sejalrai23@gmail.com>
2022-06-09 13:34:51 +0530

    Time: 8 ms (39.38%), Space: 11.2 MB (14.30%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    string ans;
-     unordered_map<string , int> mp;
-   void solve(string &s,int n){
-		if(s.size()==n){
-			// cout<<s<<" ";
-			 if(mp.find(s)==mp.end())
-				  ans=s;
-			return ;
-		}
-		s+='0';
-		solve(s,n);
-		s.pop_back(),s+='1';
-		solve(s,n);
-		s.pop_back();
-	}
-	string findDifferentBinaryString(vector<string>& nums) {
+    string ans="";
+   void dfs(map<string , int> &mp , int n ,int i, string res){
+       if(ans!="") return ;
+       if( res.length()==n || i==n){
+           // cout<<res<<endl;
+           if(mp[res]==0){
+               ans=res;
+               return;
+               
+           } 
+           return;
+       }
+       res+='0';
+       dfs(mp , n , i+1 , res);
+       res.pop_back();
+       res+='1';
+       dfs(mp , n , i+1 , res);
+       res.pop_back();
        
-		for(auto &ele: nums){
-			 mp[ele]++;
-		}
-		string s;
-		solve(s,nums.size());
-		return ans;
-	}
+   }
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n= nums[0].length();
+       map<string , int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        // cout<<mp.size()<<endl;
+        dfs(mp , n , 0 , "");
+        return ans;
+        
+        
+        
+        
+    }
 };
