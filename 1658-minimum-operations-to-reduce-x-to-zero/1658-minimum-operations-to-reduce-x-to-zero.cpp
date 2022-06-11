dbcaae883f4f1f78f75167f1dd5834fe8ca71c21
Sejal Rai <sejalrai23@gmail.com>
2022-06-11 11:06:34 +0530

    Time: 189 ms (85.87%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -8,22 +8,27 @@ public:
         }
         sum=sum-x;
         if(sum<0) return -1;
+        if(sum==0) return nums.size();
         int i=0,j=0, req=0, flag=0, maxi=0;
         while(j<nums.size()){
             req+=nums[j];
-            // if(req<sum){
-            //     j++;
-            // }
-            while(req>sum){
-               req-=nums[i];
-                i++;
-            }
-            if(req==sum){
-                maxi=max(maxi,j-i+1);
+            if(req<sum){
+                j++;
+            }else if(req==sum){
+                maxi=max(maxi, j-i+1);
                 flag=1;
+                j++;
+            }else if(req> sum){
+                while(req>sum){
+                    req-=nums[i];
+                    i++;
+                }
+                if(req==sum){
+                    maxi=max(maxi,j-i+1);
+                    flag=1;
+                }
+                j++;
             }
-            j++;
-            
         }
         // cout<<sum<<"-"<<maxi<< "-"<<flag<<endl;
         if(flag){
