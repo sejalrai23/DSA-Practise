9a1754fc40a259adf26a9957377061bdc8899226
Sejal Rai <sejalrai23@gmail.com>
2022-06-13 23:38:27 +0530

    Added solution - LeetHub

---
+++ nCr - GFG/n-cr.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    
+    int nCr(int n, int r){
+        // code here
+        if(n<r) return 0;
+        if((n-r)< r) r=n-r;
+        int mod=1e9+7;
+        vector<int> dp(r+1 , 0);
+        dp[0]=1;
+        
+        for(int i=1;i<=n ;i++){
+            for(int j=min(i,r);j>0 ; j--){
+                dp[j]=(dp[j]+dp[j-1])%mod;
+            }
+        }
+        
+        return dp[r];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, r;
+        cin>>n>>r;
+        
+        Solution ob;
+        cout<<ob.nCr(n, r)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
