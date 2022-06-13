d8b47414f64e25f494dddf959e467f57f651f1bf
Sejal Rai <sejalrai23@gmail.com>
2022-06-14 00:39:36 +0530

    Added solution - LeetHub

---
+++ Friends Pairing Problem - GFG/friends-pairing-problem.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    int countFriendsPairings(int n) 
+    { 
+        // code here
+        int mod=1e9+7;
+        long long dp[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n ;i++){
+            dp[i]=(dp[i-1]+ ((i-1)*dp[i-2]))%mod;
+        }
+       return dp[n]%mod;
+    }
+};    
+ 
+
+// { Driver Code Starts.
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout <<ob.countFriendsPairings(n); 
+    	cout<<endl;
+    }
+} 
+  // } Driver Code Ends
