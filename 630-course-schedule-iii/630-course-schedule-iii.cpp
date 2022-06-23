5c5e4f447c39ae93c6157ea82cbbf2329e7b7ba1
Sejal Rai <sejalrai23@gmail.com>
2022-06-23 15:36:52 +0530

    Time: 2871 ms (5.09%), Space: 272.4 MB (5.74%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -8,21 +8,16 @@ public:
         int x=0, cnt=0;
         priority_queue<int> pq;
         for(auto it: courses){
-            // cout<<it[0]<<"-"<<it[1]<<endl;
             if(it[0]<=it[1]){
             x+= it[0];
             cout<<x<<endl;
             if(x<=it[1]){
-                 // cout<<it[0]<<"-"<<it[1]<<endl;
                 cnt++;
                 pq.push(it[0]);
             }else{
                 if(pq.top()>it[0]){
-                    // cout<<pq.top()<<endl;
                     x-=pq.top();
                     pq.pop();
-                    // cnt--;
-                    // x+=it[0];
                     pq.push(it[0]);
                 }else{
                     x-=it[0];
