2a90c453839e72c9139f0f07db71d89e1b220948
Sejal Rai <sejalrai23@gmail.com>
2022-06-29 15:27:16 +0530

    Time: 187 ms (50.20%), Space: 12.2 MB (46.32%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -4,14 +4,23 @@ public:
         if(a[0]==b[0]){
             return a[1]<b[1];
         }
-        return a[0]>b[0];
+        return a[0]<b[0];
     }
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         sort(people.begin(),people.end(),cmp);
-        vector<vector<int>> ans;
+        vector<vector<int>> ans(people.size(), {-1,-1});
         for(auto it : people){
-            int idx=it[1];
-            ans.insert(ans.begin()+idx , it);
+            cout<<it[0]<<"-"<<it[1]<<endl;
+            int idx= it[1];
+            for(int i=0;i<people.size();i++){
+                if(ans[i][0]== -1 && idx==0){
+                    ans[i][0]=it[0];
+                    ans[i][1]=it[1];
+                    break;
+                }else if(ans[i][0]== -1 || ans[i][0] >=it[0]){  //decrease idx only when the gap is empty or it is filled with some other person with greater than or euql height
+                    idx--;
+                }
+            }
         }
         return ans;
        
