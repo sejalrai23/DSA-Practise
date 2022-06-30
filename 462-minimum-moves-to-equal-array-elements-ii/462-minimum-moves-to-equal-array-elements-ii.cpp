e7774508d6f6a0b805cba0cf7e4859a5dd28e1ac
Sejal Rai <sejalrai23@gmail.com>
2022-06-30 12:58:37 +0530

    Time: 10 ms (90.92%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
        
-        int ans=0, n=nums.size();
+        int ans=0, n=nums.size(), med=0;
         sort(nums.begin(),nums.end());
-        int med=nums[n/2];
+        if(n%2!=0){
+             med=nums[n/2];
+        }else{
+            med= (nums[(n-1)/2]+ nums[((n-1)/2)+1])/2;
+        }
+      
         for(auto it : nums){
            
             ans+= abs(it-med);
