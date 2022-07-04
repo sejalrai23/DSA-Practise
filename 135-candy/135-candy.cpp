3773f139a56e43e3c5a6769f0cb192b28cec23cb
Sejal Rai <sejalrai23@gmail.com>
2022-07-05 00:20:37 +0530

    Time: 52 ms (15.52%), Space: 17.8 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -3,10 +3,13 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> arr(n, 1);
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++){
             if (ratings[i] > ratings[i - 1]) arr[i] = arr[i - 1] + 1;
-        for (int i = n - 2; ~i; i--)
+        }
+        
+        for (int i = n - 2; i>=0 ; i--){
             if (ratings[i] > ratings[i + 1]) arr[i] = max(arr[i], arr[i + 1] + 1);
+        }
         
         
         int res = 0;
