f6094cc058c66e9ab986cb2d967f415ffb5b99c2
Sejal Rai <sejalrai23@gmail.com>
2022-07-07 00:38:17 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+   Node* final=new Node(-1);
+   Node* ans=final;
+   Node* temp1=head1;
+   Node* temp2=head2;
+   
+   while(temp1 && temp2){
+       if(temp1->data < temp2->data) temp1=temp1->next;
+       else if(temp2->data < temp1->data) temp2=temp2->next;
+       else{
+           ans->next = new Node(temp1->data);
+           ans=ans->next;
+           temp1=temp1->next;
+           temp2=temp2->next;
+       }
+   }
+   return final->next;
+}
