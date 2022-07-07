e7c0acdcc8b46d9e593a336c418929ceb81097a7
Sejal Rai <sejalrai23@gmail.com>
2022-07-08 03:26:53 +0530

    Time: 15 ms (99.64%), Space: 13.9 MB (23.88%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,22 +11,32 @@
 
     class Solution {
 public:
-    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-        ListNode* head = l1->val <= l2->val? l1 : l2;
-        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
-        return head;
-    }
+
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.size() == 0) return NULL;
         
-        ListNode* head = lists[0];
+        priority_queue< int , vector<int> , greater<int>> pq;
         
-        for (int i = 1; i < lists.size(); i++)
-            head = merge2Lists(head, lists[i]);
         
-        return head;
+        for (int i = 0; i < lists.size(); i++){
+            ListNode* temp=lists[i];
+            while(temp){
+                pq.push(temp->val);
+                temp=temp->next;
+            }
+        }
+        
+        ListNode* ans=new ListNode(-1);
+        ListNode* dum=ans;
+        // cout<<pq.size()<<endl;
+        while(!pq.empty()){
+            dum->next= new ListNode(pq.top());
+            pq.pop();
+            dum=dum->next;
+        }
+            
+        
+        return ans->next;
     }
 };
