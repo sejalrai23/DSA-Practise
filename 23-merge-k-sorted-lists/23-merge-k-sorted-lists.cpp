11515beba2612b86b3e66036f1fc4633d31f16b4
Sejal Rai <sejalrai23@gmail.com>
2022-07-08 03:10:09 +0530

    Time: 264 ms (20.61%), Space: 13.3 MB (74.51%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,38 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+
+    class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
-        if(l1==NULL || l2==NULL){
-            return l1==NULL ? l2 : l1;
-        }
-        ListNode* v1=new ListNode();
-        if(l1->val <= l2->val){
-            v1->val = l1->val;
-            v1->next= mergeTwoLists(l1->next , l2);
-        }
-         else{
-            v1->val=l2->val;
-            v1->next= mergeTwoLists(l1 , l2->next);
-        }
-        return v1;
-        
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
     }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0){
-            return NULL;
-        }
-        // ListNode* ans=lists[0] ;
-        // for(int i =1 ; i<lists.size() ; i++){
-        //     ans = mergeTwoLists(ans, lists[i]);
-        // }
-        while(lists.size() > 1){
-        lists.push_back(mergeTwoLists(lists[0], lists[1]));
-        lists.erase(lists.begin());
-        lists.erase(lists.begin());
-        }
     
-        return lists[0];
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
     }
 };
