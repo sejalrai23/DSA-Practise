3b7477c2eb748c78bb90d5d00a239b6f560957eb
Sejal Rai <sejalrai23@gmail.com>
2022-07-19 03:55:55 +0530

    Time: 168 ms (30.71%), Space: 50.1 MB (5.42%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int cum=0; // cumulated sum
+       int cum=0; // cumulated sum
         map<int,int> rec; // prefix sum recorder
         int cnt = 0; // number of found subarray
         rec[0]++; // to take into account those subarrays that begin with index 0
