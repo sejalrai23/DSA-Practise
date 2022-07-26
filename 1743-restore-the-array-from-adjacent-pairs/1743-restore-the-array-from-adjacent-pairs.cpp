d5ae636a920fbed7c4789b93e52bbaebcc764cc7
Sejal Rai <sejalrai23@gmail.com>
2022-07-26 20:23:57 +0530

    Time: 923 ms (54.92%), Space: 163.4 MB (21.17%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> ans;
-    void dfs(map<int , vector<int>> &mp, map<int, int>& vis , int node){
+    void dfs(unordered_map<int , vector<int>> &mp, unordered_map<int, int>& vis , int node){
         vis[node]=1;
         ans.push_back(node);
         
@@ -14,14 +14,14 @@ public:
     
     vector<int> restoreArray(vector<vector<int>>& vec) {
         int n=vec.size()+1;
-        map<int, vector<int>> mp;
+        unordered_map<int, vector<int>> mp;
         
         for(auto it: vec){
             mp[it[0]].push_back(it[1]);
             mp[it[1]].push_back(it[0]);
         }
         
-        map<int , int> vis;
+        unordered_map<int , int> vis;
         int node;
         for(auto it: mp){
         if(it.second.size()==1){
