d48adc943d85eb2ced6b4e5b3dba88738267fa81
Sejal Rai <sejalrai23@gmail.com>
2022-07-27 20:30:18 +0530

    Time: 75 ms (60.08%), Space: 32.6 MB (60.88%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -14,7 +14,7 @@ public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
         vector<int> indegree(n,0);
         for (int i=0;i<n;i++) {
-            if(leftChild[i]!=-1 && ++indegree[leftChild[i]]>1){ //if incoming edges exceed 1, not possible
+            if(leftChild[i]!=-1 && ++indegree[leftChild[i]]>1){ 
                 return false;
             }
             if(rightChild[i]!=-1 && ++indegree[rightChild[i]]>1){
@@ -24,24 +24,18 @@ public:
         int root=-1;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
-                if(root!=-1){  // we see more than 1 root, not possible, return false
+                if(root!=-1){  
                     return false;
                 }
                 root=i;
             }
         }        
-        if(root==-1){  //if root is still -1 there is no root, return false
+        if(root==-1){ 
             return false;
         }
         vector<bool> vis(n,false);
         cnt=0;
         traverseTree(leftChild,rightChild,root,vis);
-        
-        // for(auto e: vis){
-        //     if(e==false){  //this node is left unvisited and hence not a valid tree, return false 
-        //         return false;
-        //     }
-        // }
         return cnt==n; 
     }
 };
