0e8255558fe8d8b6cc7cfb8dd04face2e232543f
Sejal Rai <sejalrai23@gmail.com>
2022-07-28 04:57:46 +0530

    Time: 13 ms (55.33%), Space: 9.6 MB (55.97%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int res = 0;
+        for(int i = 0; i <= 31; ++i) {
+            int check = 1 << i, count = 0;
+            for(auto child : nums) {
+                if(child & check)
+                    count += 1;
+            }
+            if(count % 3 == 1) 
+                res = res | check;
+        }
+
+        return res;
+    }
+};
