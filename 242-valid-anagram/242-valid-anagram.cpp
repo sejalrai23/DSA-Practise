07959cef2fbe3ab06485161e06dd2e54b4ba6fce
Sejal Rai <sejalrai23@gmail.com>
2022-07-28 06:10:17 +0530

    Time: 18 ms (40.58%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<int, int> mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        
+        for(int i=0;i<t.length();i++){
+            mp[t[i]]--;
+        }
+        
+        for(auto it: mp){
+            if(it.second!=0) return false;
+        }
+        return true;
+    }
+};
