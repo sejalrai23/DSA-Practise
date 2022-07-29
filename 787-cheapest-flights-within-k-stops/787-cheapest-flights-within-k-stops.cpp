bab663f28356ce5fb509c7a0f3523817b6ebd978
Sejal Rai <sejalrai23@gmail.com>
2022-07-29 21:55:29 +0530

    Time: 50 ms (54.15%), Space: 15.5 MB (30.10%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -7,8 +7,8 @@ public:
         }
         
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0 , src , 0});
-        vector<int> dist(n+1, INT_MAX);
+        pq.push({0 , src , k});
+        vector<int> dist(n+1, INT_MIN);
         while(!pq.empty()){
             auto it= pq.top();
             pq.pop();
@@ -18,12 +18,12 @@ public:
            
             if(node==dst) return cost;
             
-           if(dist[node]<stops) continue;
+           if(dist[node]>stops) continue;
             dist[node]=stops;
             
-            if(stops<=k){
+            if(stops>=0){
                 for(auto x: adj[node]){
-                    pq.push({x.second+cost , x.first, stops+1}); 
+                    pq.push({x.second+cost , x.first, stops-1}); 
                 }
             }
         }
