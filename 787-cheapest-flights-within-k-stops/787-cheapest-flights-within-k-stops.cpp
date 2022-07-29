d3fc2ea834478ac8f9100fdc2029d7e3911786a1
Sejal Rai <sejalrai23@gmail.com>
2022-07-29 21:54:20 +0530

    Time: 45 ms (61.21%), Space: 15.6 MB (28.56%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int , int>>> adj(n);
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0 , src , 0});
+        vector<int> dist(n+1, INT_MAX);
+        while(!pq.empty()){
+            auto it= pq.top();
+            pq.pop();
+            int cost= it[0];
+            int node= it[1];
+            int stops=it[2];
+           
+            if(node==dst) return cost;
+            
+           if(dist[node]<stops) continue;
+            dist[node]=stops;
+            
+            if(stops<=k){
+                for(auto x: adj[node]){
+                    pq.push({x.second+cost , x.first, stops+1}); 
+                }
+            }
+        }
+        return -1;
+    }
+};
