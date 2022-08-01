812aa9a2bb6c87b77522a5f479f466f1e3012c4c
Sejal Rai <sejalrai23@gmail.com>
2022-08-01 14:37:11 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.54%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[102][102];
+    bool isValid(int i , int j , int m , int n){
+        if(i<0 || i>=m || j<0 || j>=n) return false;
+        return true;
+    }
+    int dfs(int m , int n , int i , int j , vector<vector<int>>& vis){
+        if(!isValid(i , j , m , n) || vis[i][j]==1) return 0;
+        
+        if(i==m-1 && j==n-1) return 1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        vis[i][j]=1;
+        int down= dfs(m , n , i+1 , j, vis);
+        int right= dfs(m , n , i, j+1, vis);
+        vis[i][j]=0;
+        return dp[i][j]=right+down;
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp , -1 , sizeof(dp));
+        vector<vector<int>> vis(m ,vector<int>( n , 0));
+        return dfs(m , n , 0, 0, vis);
+    }
+};
