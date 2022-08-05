cfa9e187a9e62ac8c19379c071a73784de62b2ff
Sejal Rai <sejalrai23@gmail.com>
2022-08-05 15:47:29 +0530

    Time: 7 ms (19.94%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     
-    int recur(vector<int>& nums , int target , int ind, int n, vector<vector<int>>& dp){
+    int recur(vector<int>& nums , int target , int ind, int n, vector<int>& dp){
        if(target==0) return 1;
         
         if(ind>=n || target<0) return 0;
-        if(dp[ind][target]!=-1) return dp[ind][target];
+        if(dp[target]!=-1) return dp[target];
         int ans=0;
         
         for(int i=0;i<n ;i++){
@@ -16,11 +16,11 @@ public:
         
          
         
-        return dp[ind][target]= ans;
+        return dp[target]= ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1 , vector<int>(target+1 , -1));
+        vector<int> dp(target+1 , -1);
         return recur(nums , target , 0 , n, dp);
     }
 };
