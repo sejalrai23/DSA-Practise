3cb683010e0bff1225c2abe67e5db8de8ff5a30a
Sejal Rai <sejalrai23@gmail.com>
2022-08-11 07:19:09 +0530

    Time: 6 ms (59.12%), Space: 14 MB (30.42%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -7,7 +7,7 @@ public:
             return;
         }
         if(n>0) recur(n-1,m , s+'(');
-        if(m>n) recur(n, m-1, s+')');
+        if(m>n) recur(n, m-1, s+')'); //because we cannot only add ')' without '( '
     }
     vector<string> generateParenthesis(int n) {
         recur(n, n, "");
