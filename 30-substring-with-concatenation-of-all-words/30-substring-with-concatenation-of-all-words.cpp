f65a8422bad9ff3c90b371e08d9cd7fe1c4dd44c
Sejal Rai <sejalrai23@gmail.com>
2022-08-13 19:05:48 +0530

    Time: 2837 ms (5.04%), Space: 638.7 MB (5.02%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkSubstr(unordered_map<string , int> temp , int wordLen , int start , int windowLen , string s){
+        unordered_map<string , int>  mp=temp;
+        // cout<<start<<"-"<<start+windowLen<<endl;
+        for(int i=start;i<start+windowLen ; i+=wordLen){
+            // cout<<i<<"-"<<wordLen<<endl;
+            string sub= s.substr(i , wordLen);
+            // cout<<sub<<endl;
+            if(mp.find(sub)==mp.end()) return false;
+            else{
+                mp[sub]--;
+               
+                if(mp[sub]==0) mp.erase(sub);
+            }
+        }
+        if(mp.size()==0) return true;
+        return false;
+    }
+    
+    vector<int> findSubstring(string s, vector<string>& words) {
+       unordered_map<string , int> mp;
+        
+        for(auto it: words){
+            mp[it]++;
+        }
+        int wordLen= words[0].size();
+        int windowLen= words[0].size() * words.size();
+        
+        int i=0;
+        vector<int> res;
+        while(i+windowLen <=s.length()){
+            // cout<<i<<endl;
+            if(checkSubstr(mp , wordLen , i , windowLen, s)){
+                res.push_back(i);
+            
+            }
+            i++;
+        }
+        return res;
+        
+    }
+};
