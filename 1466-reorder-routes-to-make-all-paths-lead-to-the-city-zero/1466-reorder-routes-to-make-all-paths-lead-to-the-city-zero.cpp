b5235ba30afbe26aa8e73055d61dd56456b847f1
Sejal Rai <sejalrai23@gmail.com>
2022-08-27 19:48:49 +0530

    Time: 796 ms (33.35%), Space: 102.5 MB (86.09%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    
-    void dfs(int node,int& cnt , vector<int>& vis,vector<int> adj[]){
+    int cnt =0;
+    void dfs(int node, vector<int>& vis,vector<int> adj[]){
         vis[node]=1;
         for(auto& it : adj[node]){
             if(!vis[abs(it)]){
-                if(it>0){
-                    cnt++;}
-                dfs(abs(it),cnt,vis,adj);    
-                
-               
+                if(it>0){cnt++;};
+                dfs(abs(it),vis,adj);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& con) {
         vector<int> vis(n,0);
-       
-
-         vector<int> adj[n];
+        vector<int> adj[n];
         for (int i=0;i<con.size();i++){
             adj[con[i][0]].push_back(con[i][1]);
             adj[con[i][1]].push_back(-con[i][0]);
         }
-         int cnt=0;
-        dfs(0,cnt,vis, adj);
+               
+        dfs(0,vis, adj);
+       
         
         return cnt;
     }
