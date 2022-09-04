bd8166f1a6e56d0c4b84fd6c5736267fd2c6017b
Sejal Rai <sejalrai23@gmail.com>
2022-09-04 19:47:20 +0530

    Time: 8 ms (58.22%), Space: 12 MB (88.48%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,33 +11,25 @@
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode* , pair<int , int>>> q;
-        map<int , map<int , multiset<int>>> mp;
-        q.push({root , {0 , 0}});
-        
-        while(!q.empty()){
-            auto p= q.front();
-            q.pop();
-            TreeNode* node= p.first ;
-            int x= p.second.first;
-            int y= p.second.second;
-            
-            mp[x][y].insert(node->val);
-            
-            if(node->left) q.push({node->left ,{x-1 , y+1}});
-            
-            if(node->right) q.push({node->right ,{x+1 , y+1}});
-        }
-        
-        vector<vector<int>> ans;
-        for(auto it: mp){
-            vector<int> col;
-            for(auto x: it.second){
-                col.insert(col.end(), x.second.begin(), x.second.end());
-            }
-            ans.push_back(col);
-        }
-        return ans;
-    }
+     vector<vector<int>> verticalTraversal(TreeNode* root) {
+	    map<int, vector<int> > m;  
+	    queue<pair<int, TreeNode*> > q; 
+	    q.push(make_pair(0, root));  
+	    while(!q.empty()) {
+		   multiset<pair<int,int>> tmp; 
+		    int len=q.size();
+		    for(int i=0;i<len;++i) {
+			     auto p=q.front();q.pop();
+			     tmp.insert(make_pair(p.first, p.second->val));
+			     if (p.second->left) q.push(make_pair(p.first-1, p.second->left));
+			     if (p.second->right) q.push(make_pair(p.first+1, p.second->right));
+		    }
+
+		    for(auto p : tmp) m[p.first].push_back(p.second);
+	   }
+
+	   vector<vector<int> > res;
+	   for(auto x : m) res.push_back(x.second);
+	   return res;
+  }
 };
