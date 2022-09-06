4c529666f88407c3ee5f55053df4ec16e30649ab
Sejal Rai <sejalrai23@gmail.com>
2022-09-06 19:28:07 +0530

    Time: 43 ms (40.34%), Space: 27.9 MB (15.01%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -13,6 +13,7 @@ class Solution {
 public:
     vector<TreeNode*> trees;
     map<int, int> mp;
+    
     TreeNode* findandDelete(TreeNode* & root){
         if(root==NULL) return NULL;
         TreeNode* left= findandDelete(root->left );
@@ -35,31 +36,21 @@ public:
         return NULL;
     }
     
-    // void pushTrees(TreeNode* nodes , vector<TreeNode*> &ans){
-    //     if(root==NULL) return;
-    //     ans.push
-    // }
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        // map<int, int> mp;
         
         for(auto it: to_delete){
             mp[it]=1;
         }
-        
-       
         TreeNode* rootx=findandDelete(root);
-           vector<TreeNode*> ans;
+        vector<TreeNode*> ans;
+        
         if(rootx==root){
            if(root->right)  ans.push_back(root->right);
            if(root->left)  ans.push_back(root->left);
-            // return ans;
         }else{
              ans.push_back(root);
         }
     
-     
-       
-        
         for(auto it: trees){
             if(it->left){
               ans.push_back(it->left);  
@@ -68,6 +59,7 @@ public:
                ans.push_back(it->right);
             }
         }
+        
         return ans;
     }
 };
