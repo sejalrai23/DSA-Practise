466150d054b293945c53341c96efb8633d6ec64a
Sejal Rai <sejalrai23@gmail.com>
2022-09-06 19:27:04 +0530

    Time: 43 ms (40.34%), Space: 27.9 MB (14.14%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> trees;
+    map<int, int> mp;
+    TreeNode* findandDelete(TreeNode* & root){
+        if(root==NULL) return NULL;
+        TreeNode* left= findandDelete(root->left );
+        TreeNode* right= findandDelete(root->right);
+        
+        if(left) {
+            trees.push_back(root->left);
+            root->left=NULL;
+        }
+        if(right){
+            trees.push_back(root->right);
+            root->right=NULL;
+
+        }
+        
+        if(mp[root->val]){
+            mp[root->val]=0;
+            return root;
+        }
+        return NULL;
+    }
+    
+    // void pushTrees(TreeNode* nodes , vector<TreeNode*> &ans){
+    //     if(root==NULL) return;
+    //     ans.push
+    // }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        // map<int, int> mp;
+        
+        for(auto it: to_delete){
+            mp[it]=1;
+        }
+        
+       
+        TreeNode* rootx=findandDelete(root);
+           vector<TreeNode*> ans;
+        if(rootx==root){
+           if(root->right)  ans.push_back(root->right);
+           if(root->left)  ans.push_back(root->left);
+            // return ans;
+        }else{
+             ans.push_back(root);
+        }
+    
+     
+       
+        
+        for(auto it: trees){
+            if(it->left){
+              ans.push_back(it->left);  
+            }
+            if(it->right){
+               ans.push_back(it->right);
+            }
+        }
+        return ans;
+    }
+};
