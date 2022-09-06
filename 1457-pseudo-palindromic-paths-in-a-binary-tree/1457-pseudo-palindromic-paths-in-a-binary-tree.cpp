14b980687d161d08b48358e2808384cfa2a2f782
Sejal Rai <sejalrai23@gmail.com>
2022-09-07 01:02:25 +0530

    Time: 793 ms (23.59%), Space: 190.6 MB (40.87%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    void countPalindrome(TreeNode* root , map<int, int>& mp ){
+        if(root==NULL) return;
+        if(!root->left && !root->right){
+            mp[root->val]++;
+            int cnt=0;
+            for(auto it: mp){
+                // cout<<it.first<<"-"<<it.second<<endl;
+                if(it.second%2!=0){
+                    cnt++;
+                }
+            }
+        
+            mp[root->val]--;
+            if(cnt<=1) ans++;
+            // cout<<"*"<<endl;
+            return;
+        }
+        mp[root->val]++;
+        countPalindrome(root->left , mp);
+        countPalindrome(root->right , mp);
+        mp[root->val]--;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        map<int , int> mp;
+        countPalindrome(root , mp);
+        return ans;
+    }
+};
