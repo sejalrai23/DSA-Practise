b81bb880378661dee03317a3457cef9c2c51bdb2
Sejal Rai <sejalrai23@gmail.com>
2022-09-17 03:03:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (68.34%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,21 +1,23 @@
-class Solution {
+
+ class Solution {
 public:
-   string smallestNumber(string pattern) {
-        string result;
-        stack<int> s;
-        for (int i = 0; i <= pattern.length(); i++)
-        {
-            s.push(i + 1);
-            if (i == pattern.length() || pattern[i] == 'I')
-            {
-                while (!s.empty())
-                {
-                    result += to_string(s.top());
-                    s.pop();
-                }
+    string smallestNumber(string pattern) {
+        
+        int n = pattern.size();
+        
+        string num = "";
+
+        int prev = 0;
+        
+        for(int i=0; i<=n; i++) {
+            num += (i + '1');
+            if(i == n || pattern[i] == 'I') {
+                reverse(num.begin() + prev, num.end());
+                prev = i+1;
             }
+           
         }
         
-        return result;
+        return num;
     }
 };
