1025aa5ee4f89c3813caa6bcefd1effaf8bac606
Sejal Rai <sejalrai23@gmail.com>
2022-09-24 02:41:13 +0530

    Time: 81 ms (14.63%), Space: 26.5 MB (89.12%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,38 +1,28 @@
-// From constraints of the problem -10^5 <= nums[i] <= 10^5
-# define MAX_VAL 100001
+
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n=nums.size();
-        int start=-1,end=n;        
-        // Since, the array needs to be in ascending order.
-        
-        // PASS 1
-        // Iterate from the end, and check weather the element at index "i" is the current minimum element or not. If not then "i" is the starting index.
+        int start=0,end=-1;        
         int curr_min=INT_MAX;
-        for(int i = n - 1; i>=0; i--) {
-            if(curr_min > nums[i]) curr_min = nums[i];
-            
-            else if(nums[i] > curr_min) start = i;
-        }
-        
-        cout<<start<<endl;
-        
-        // PASS 2
-        // Iterate from the start, and check weather the element at index "i" is the current maximum element or not. If not then "i" is the ending index.
+        int i=0 , j=n-1;
         int cur_max = INT_MIN;
-        for(int i=0; i<n; i++) {
-            if(cur_max < nums[i]) cur_max = nums[i];
+
+        
+        while(j>=0 && i<n){
+            if(curr_min>nums[j]) curr_min=nums[j];
+            else if(nums[j]>curr_min) start=j;
             
-            else if(nums[i] < cur_max) end = i;
+            if(cur_max<nums[i]) cur_max=nums[i];
+            else if(nums[i]<cur_max) end=i;
+            
+            j--;
+            i++;
         }
         
-        cout<<end<<endl;
-        
-        // if no starting index found, that means the array is already sorted in ascending order
-        if(start==-1){ 
+        if(start==-1 || end==-1){ 
             return 0;
         }
-        return end-start+1; // number of elements between start and end index
+        return end-start+1; 
     }
 };
