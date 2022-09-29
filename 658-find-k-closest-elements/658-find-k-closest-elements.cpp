634027668b697ad7b5edc204ed0a14d258c1fb9a
Sejal Rai <sejalrai23@gmail.com>
2022-09-30 02:27:58 +0530

    Time: 97 ms (58.02%), Space: 30.9 MB (98.29%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {        
-        int n = size(arr), R = lower_bound(begin(arr), end(arr), x) - begin(arr), L = R - 1;
-        cout<<R<<endl;
-        while(k--) {
-           if(R >= n || L >= 0 && x - arr[L] <= arr[R] - x) {
-                  // cout<<"L"<<"-"<<L<<endl;
-                   L--; 
-           }
-           else{
-                // cout<<"R"<<"-"<<R<<endl;
-               R++;
-           }  
-        }
-        cout<<L<<"-"<<R<<endl;                                              
+        int n = size(arr), R = lower_bound(begin(arr), end(arr) - k, x) - begin(arr), L = R - 1;
+        // cout<<R<<endl;
+        while(k--) 
+            if(R >= n || L >= 0 && x - arr[L] <= arr[R] - x) L--;
+            else R++;
         return vector<int>(begin(arr) + L + 1, begin(arr) + R);
     }
 };
