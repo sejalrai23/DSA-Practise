fd4bb5bc38e97f1b5870407c17e8990bd3235a7d
Sejal Rai <sejalrai23@gmail.com>
2022-10-01 18:12:03 +0530

    Time: 6 ms (99.65%), Space: 10.5 MB (76.11%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int> >> pq;
-        for(int i=0;i<mat.size();i++){
-            int cnt =0;
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j]==1){
-                    cnt++;
-                }
+    
+  int calculateSoldierCount(vector<int>& v) {
+        int l=0; int h=v.size()-1;
+        while(l<=h) {
+            int mid=l+(h-l)/2;
+            if(v[mid]==0) {
+                h=mid-1;
+            } else {
+                l=mid+1;
             }
+        }
+        return l;
+    }
+
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue <pair<int,int> > pq;
+        for(int i=0;i<mat.size();i++){
+           int cnt=calculateSoldierCount(mat[i]);
             pq.push(make_pair(cnt , i));
+            if(pq.size()>k){
+                // cout<<pq.top().first<<"-"<<pq.top().second<<endl;
+                pq.pop();
+            }
         }
         vector<int> ans;
         while(k--){
             ans.push_back(pq.top().second);
             pq.pop();
         }
+        reverse(ans.begin(), ans.end());
         return ans;
         
     }
 };
+
+
+
+
+
