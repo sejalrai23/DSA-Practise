7046f099120f9bdc82838a60d10998481af1e886
Sejal Rai <sejalrai23@gmail.com>
2022-10-10 18:55:47 +0530

    Time: 4 ms (29.56%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        // int i=0 , j=palindrome.length()-1;
+        if(s.length()==1) return "";
+        bool res=false;
+        int first=0 , last=0;
+        if(s.length()%2==0){
+            first=s.length()/2-1;
+            last=s.length()/2;
+        }else if(s.length()%2!=0){
+            first=s.length()/2-1;
+            last=s.length()/2+1;
+        }
+        
+        for(int i=0;i<=first;i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                res=true;
+                break;
+            }
+        }
+        if(res) return s;
+        
+        for(int i=last;i<s.length();i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                res=true;
+                break;
+            }
+        }
+        if(res) return s;
+        else if(!res){
+            s[s.length()-1]='b';
+            return s;
+        }
+        return "";
+    }
+};
